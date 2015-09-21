1d6e735d7f8a913f79b7e3aaf87f3f5f392f956d
Sean Rhone <espionage724@tutanota.com>
2015-09-21 18:29:06 -0400

    Create config.php
    - Blank configuration template
    - Change CHANGEME spots with whatever you want

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+// Database settings
+define('DB_HOST', 'CHANGEME');
+define('DB_USER', 'CHANGEME');
+define('DB_PASS', 'CHANGEME');
+define('DB_NAME', 'auth');
+
+// Soap settings
+define('SOAP_IP', 'CHANGEME');
+define('SOAP_PORT', '7878');
+define('SOAP_USER', 'CHANGEME');
+define('SOAP_PASS', 'CHANGEME');
+
+// Misc settings
+define('CHECK_FOR_DUPLICATE_EMAIL', true);
+define('SITE_TITLE', "CHANGEME");
+define('META_DESCRIPTION', "CHANGEME");
+define('META_KEYWORDS', "CHANGEME");
+define('META_ROBOTS', "INDEX,NOFOLLOW");
+define('REALMLIST', "CHANGEME");
