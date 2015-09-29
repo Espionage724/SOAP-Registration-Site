a516e8f013a06668216457c1483bb0ba5b1c4316
Sean Rhone <espionage724@tutanota.com>
2015-09-28 20:55:01 -0400

    Update config.php
    - Added comments so people know what settings do what

--- config.php
+++ config.php
@@ -1,20 +1,45 @@
 <?php
-// Database settings
+
+// Location of MySQL server
 define('DB_HOST', 'CHANGEME');
+
+// MySQL user for modifying auth database
 define('DB_USER', 'CHANGEME');
+
+// MYSQL user password
 define('DB_PASS', 'CHANGEME');
+
+// Database that stores authentication information (usually 'auth' on TrinityCore)
 define('DB_NAME', 'auth');
 
-// Soap settings
+// Location of SOAP (worldserver with TrinityCore) server
 define('SOAP_IP', 'CHANGEME');
+
+// Port for SOAP server
 define('SOAP_PORT', '7878');
+
+// Account name to handle SOAP commands (needs RBAC permissions 219 and 228)
 define('SOAP_USER', 'CHANGEME');
+
+// Account name password
 define('SOAP_PASS', 'CHANGEME');
 
-// Misc settings
-define('CHECK_FOR_DUPLICATE_EMAIL', true);
-define('SITE_TITLE', "CHANGEME");
-define('META_DESCRIPTION', "CHANGEME");
-define('META_KEYWORDS', "CHANGEME");
-define('META_ROBOTS', "INDEX,NOFOLLOW");
+// WoW Server Address (to tell people what to enter in realmlist/Config.wtf
+// Has nothing to do with account creation directly
 define('REALMLIST', "CHANGEME");
+
+// Check if Email Address is already in-use
+define('CHECK_FOR_DUPLICATE_EMAIL', false);
+
+// Title of the site (something like "Realm of Espionage WotLK Server Registration Page")
+// This is displayed at the top of the browser window and in tabs
+define('SITE_TITLE', "CHANGEME");
+
+// Site meta description (for search engines)
+define('META_DESCRIPTION', "CHANGEME");
+
+// Site meta keywords (for search engines)
+define('META_KEYWORDS', "CHANGEME");
+
+// Site meta robots (for search engines)
+define('META_ROBOTS', "INDEX,NOFOLLOW");
